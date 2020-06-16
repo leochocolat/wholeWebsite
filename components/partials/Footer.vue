dfd50378fefe43ea9d7631faf160c5ec937a2cc9
leochocolat <l.mouraire@outlook.fr>
2020-06-16 21:37:07 +0200

    review footer

--- components/partials/Footer.vue
+++ components/partials/Footer.vue
@@ -2,47 +2,47 @@
     <footer class="footer" data-scroll data-scroll-offset="100">
         <div class="footer__container container">
             <div class="footer__content">
-                <div class="footer__col">
-                    <div class="footer__project-name">
-                        Whole
-                    </div>
-                </div>
-                <div class="footer__col">
-                    <div class="footer__team-title">
-                        Design
-                    </div>
-                    <ul class="footer__team-list">
-                        <li class="footer__team-list-item">
-                            <a href="http://serge-bocancea.fr" target="_blank" noopener class="footer__team-link">Serge Bocancea</a>
-                        </li>
-                        <li class="footer__team-list-item">
-                            <a href="http://serge-bocancea.fr" target="_blank" noopener class="footer__team-link">Serge Bocancea</a>
-                        </li>
-                        <li class="footer__team-list-item">
-                            <a href="http://serge-bocancea.fr" target="_blank" noopener class="footer__team-link">Serge Bocancea</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="footer__col">
-                    <div class="footer__team-title">
+                <div class="footer__credits footer__credits--dev">
+                    <div class="footer__credits-title">
                         Développement
                     </div>
-                    <ul class="footer__team-list">
-                        <li class="footer__team-list-item">
-                            <a href="http://serge-bocancea.fr" target="_blank" noopener class="footer__team-link">Serge Bocancea</a>
+                    <ul class="footer__credits-list">
+                        <li class="footer__credits-list-item">
+                            <a href="" target="_blank" noopener="true" class="footer__credits-link">
+                                Serge Bocancea
+                            </a>
                         </li>
-                        <li class="footer__team-list-item">
-                            <a href="http://serge-bocancea.fr" target="_blank" noopener class="footer__team-link">Serge Bocancea</a>
+                        <li class="footer__credits-list-item">
+                            <a href="" target="_blank" noopener="true" class="footer__credits-link">
+                                Brieuc Caillot
+                            </a>
                         </li>
-                        <li class="footer__team-list-item">
-                            <a href="http://serge-bocancea.fr" target="_blank" noopener class="footer__team-link">Serge Bocancea</a>
+                        <li class="footer__credits-list-item">
+                            <a href="" target="_blank" noopener="true" class="footer__credits-link">
+                                Léo Mouraire
+                            </a>
                         </li>
                     </ul>
-                </div>
-                <div class="footer__col">
-                    <div class="footer__logo-gobelins-container">
-                        <img src="https://www.gobelins.fr/sites/all/themes/custom/gobelins_theme/build/images/logo-gobelins.png" alt="" class="footer__logo-gobelins">
+                </div
+                ><div class="footer__credits footer__credits--design">
+                    <div class="footer__credits-title">
+                        Design
                     </div>
+                    <ul class="footer__credits-list">
+                        <li class="footer__credits-list-item">
+                            <a href="" target="_blank" noopener="true" class="footer__credits-link">
+                                Emma Rimbert
+                            </a>
+                        </li>
+                        <li class="footer__credits-list-item">
+                            <a href="" target="_blank" noopener="true" class="footer__credits-link">
+                                Pauline Fuchs
+                            </a>
+                        </li>
+                    </ul>
+                </div
+                ><div class="footer__gobelins-logo-container">
+                    <img src="https://www.gobelins.fr/sites/all/themes/custom/gobelins_theme/build/images/logo-gobelins.png" alt="" class="footer__gobelins-logo">
                 </div>
             </div>
             <div class="footer__legals">
